46308685b4895b38476d9df3ae14d48f290a1119
Jada Overmars <jkovermars@gmail.com>
2023-07-19 10:02:24 -0600

    Changed VAE into simple autoencoder by removing second innermost layer of neural network and encoding x into single point rather than probability distribution

--- main.py
+++ main.py
@@ -54,45 +54,38 @@ class VAE(nn.Module):
         super(VAE, self).__init__()
 
         self.fc1 = nn.Linear(784, 256)
-        self.fc21 = nn.Linear(256, 2)
-        self.fc22 = nn.Linear(256, 2)
+        self.fc2 = nn.Linear(256, 2)
         self.fc3 = nn.Linear(2, 256)
         self.fc4 = nn.Linear(256, 784)
 
     def encode(self, x):
         h1 = F.relu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1)
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return mu + eps*std
+        return self.fc2(h1)
 
     def decode(self, z):
         h3 = F.relu(self.fc3(z))
         return torch.sigmoid(self.fc4(h3))
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 784))
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        z = self.encode(x.view(-1, 784))
+        return self.decode(z), z
 
 
 model = VAE().to(device)
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 
-# Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar):
+# Only reconstruction loss
+def loss_function(recon_x, x):
     BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
 
     # see Appendix B from VAE paper:
     # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
     # https://arxiv.org/abs/1312.6114
     # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    # KLD = -0.5 * torch.sum(1 + logvar -git mu.pow(2) - logvar.exp())
 
-    return BCE + KLD
+    return BCE #+ KLD
 
 train_losses = []
 def train(epoch):
@@ -101,8 +94,8 @@ def train(epoch):
     for batch_idx, (data, _) in enumerate(train_loader):
         data = data.to(device)
         optimizer.zero_grad()
-        recon_batch, mu, logvar = model(data)
-        loss = loss_function(recon_batch, data, mu, logvar)
+        recon_batch, _ = model(data)
+        loss = loss_function(recon_batch, data)
         loss.backward()
         train_loss += loss.item()
         optimizer.step()
@@ -123,15 +116,15 @@ def train(epoch):
 def test(epoch):
     model.eval()
     test_loss = 0
-    latent_vectors = [] # ***
-    labels = [] # ***
+    latent_vectors = []
+    labels = []
     with torch.no_grad():
         for i, (data, target) in enumerate(test_loader):
             data = data.to(device)
-            recon_batch, mu, logvar = model(data)
-            latent_vectors.append(mu.cpu().numpy()) # ***
-            labels.extend(target.numpy()) # ***
-            test_loss += loss_function(recon_batch, data, mu, logvar).item() 
+            recon_batch, z = model(data)
+            latent_vectors.append(z.cpu().numpy())
+            labels.extend(target.numpy())
+            test_loss += loss_function(recon_batch, data).item() 
             if i == 0:
                 n = min(data.size(0), 8)
                 comparison = torch.cat([data[:n],
